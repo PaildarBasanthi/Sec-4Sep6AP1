a559eae23aa7791e967fb213737832acc8e57067
PaildarBasanthi <basanthisrec@gmail.com>
2023-09-07 14:56:33 +0530

    first Commit

---
+++ Phase3Section2.21/Models/GenericDBContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Phase3Section2._32.Models
+{
+    public class GenericDBContext : DbContext
+    {
+        public GenericDBContext()
+           : base("School1Entities")
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
+
